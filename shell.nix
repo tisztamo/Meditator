6d14d477ac7c5a1ccc06f84cc885812baa5450f4
Krisztian Schaffer <tisztamo@github.com>
2025-01-31 16:48:36 +0100

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/0bf3109eeb61780965c27f4a0a4affdcd0cd4d3d.tar.gz") {}
+}:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.git
+    pkgs.tmux
+    pkgs.htop
+    pkgs.nodejs
+  ];
+
+  shellHook = ''
+    HEAD_COLOR="\033[0;32m";
+    NO_COLOR="\033[0m";
+
+    printf "$HEAD_COLOR\n"
+    echo "------------- StreamOfConsciousness dev environment------------"
+    echo "---------------------------------------------------------------"
+    printf "$NO_COLOR"
+
+  '';
+
+}
