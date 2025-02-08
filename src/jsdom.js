fecd2ce6278ec3bcd9634d1bcecd024f209e5273
Krisztián Schäffer <schaffer.krisztian@ardinsys.eu>
2025-02-08 06:01:20 +0100

    Export Document from jsdom to globalThis

--- src/jsdom.js
+++ src/jsdom.js
@@ -5,6 +5,7 @@ const { window } = new JSDOM('<!doctype html><html><body></body></html>');
 
 globalThis.window = window;
 globalThis.document = window.document;
+globalThis.Document = window.Document;
 globalThis.navigator = window.navigator;
 globalThis.HTMLElement = window.HTMLElement
 globalThis.customElements = window.customElements
